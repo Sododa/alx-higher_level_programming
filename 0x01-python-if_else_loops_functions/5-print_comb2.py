eb6c341f7157dc0d98a36364ee219ba797608e45
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-02 19:28:09 +0000

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{:d}, '.format(num), end='')
-    print('99')
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
