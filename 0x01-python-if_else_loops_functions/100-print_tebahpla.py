7e9ed56a2522a09db452a51f6167bbd0be5310b1
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-02 19:35:37 +0000

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{}".format(chr(i) + chr(i - 32) if i % 2 == 0 else ""), end="")
+for i in range(ord('a'), ord('z')-1, -1):
+    print(chr(i) if i % 2 == 0 else chr(i).upper(), end='')
