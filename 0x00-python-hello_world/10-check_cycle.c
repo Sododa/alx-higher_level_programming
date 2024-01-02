5da69009457e2b14549d0a7c26a1607dc817d386
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-02 19:50:55 +0000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,16 +6,17 @@
  */
 int check_cycle(listint_t *list)
 {
-listint_t *slow = list;
-listint_t *fast = list;
-if (!list)
-return (0);
-while (slow && fast && fast->next)
-{
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
-return (1);
-}
-return (0);
+listint_t *turtle = list;
+	listint_t *hare = list;
+
+	while (turtle && hare && hare->next)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+
+		if (turtle == hare)
+			return (1);
+	}
+
+	return (0);
 }
