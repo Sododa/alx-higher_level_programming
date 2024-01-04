1b455c5026dc71643987b99dccf62169bbbaf554
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-04 18:14:12 +0000

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+     """Print all names defined by hidden_4 module."""
     import hidden_4
-    name = dir(hidden_4)
-    for j in range(len(name)):
-        if name[j][0] != "_" and name[j][1] != "_":
-            print(name[j])
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
