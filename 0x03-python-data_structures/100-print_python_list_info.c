fe0fac39e97ef3e16794824bac7bb62cc8c439f3
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-08 18:08:00 +0000

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,17 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <Python.h>
+#include <stdio.h>
 
 /**
- * print_python_list_info - prints info about python lists
- * @p: address of pyobject struct
+ * print_python_list_info - gives data of the PyListObject
+ *
+ * @p: the PyObject
  */
+
 void print_python_list_info(PyObject *p)
 {
-	int j;
+        Py_ssize_t size = 0;
+        int j = 0;
 
-	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (j = 0; j < Py_SIZE(p); j++)
-		printf("Element %d: %s\n", j, Py_TYPE(PyList_GetItem(p, j))->tp_name);
+        if (PyList_CheckExact(p))
+        {
+                size = PyList_Size(p);
+
+                printf("[*] Size of the Python List = %zd\n", size);
+                printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+
+                while (j < size)
+                {
+                        printf("Element %d: %s\n",
+                                        j, Py_TYPE(PyList_GetItem(p, j))->tp_name);
+                        j++;
+                }
+        }
 }
