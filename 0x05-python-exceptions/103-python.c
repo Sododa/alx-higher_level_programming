b55dea137450f46f344d9b92dc0b4ea4850212d8
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-22 11:08:29 +0000

    103

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints  info about Python lists.
  * @p: A PyObject list object.
@@ -75,7 +79,7 @@ void print_python_bytes(PyObject *p)
 }
 
 /**
- * print_python_float - Prints basic  about Python float objects.
+ * print_python_float - Prints  info about Python float objects.
  * @p: A PyObject float object.
  */
 void print_python_float(PyObject *p)
