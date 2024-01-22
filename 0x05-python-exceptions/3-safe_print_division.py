515a8566d15b73125efd6dfdad19c5d821bd3691
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-22 10:00:49 +0000

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
+    """
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        result = a / b
+    except:
+        result = None
     finally:
-        print("Inside result: {}".format(div))
-    return (div)
+        print("Inside result: {}".format(result))
+    return result
