17d67e1bf1c7953b9ad5c7c0ed1e95aafa7c92fb
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-01-31 10:01:40 +0000

    correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""
+This is the addition module.
+a and b must be first casted
+"""
+
+
 def add_integer(a, b=98):
-    """My addition fun
-    Args:
-        a: first integer
-        b: second integer
-    Return:
-        The return a + b
+    """a and are integers
+    Returns an integer: the addition of a and b
     """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError('a must be an integer')
-    elif not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not (isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int)):
+        raise TypeError("b must be an integer")
     return (a + b)
