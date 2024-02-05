8d44ad609a8cec702efb6ca421522864910fcd38
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-02-05 17:55:46 +0000

    sruare

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,46 +1,16 @@
 #!/usr/bin/python3
-""" write a class that inherits from BaseGeometry"""
-
-
-class BaseGeometry:
-    """public instance method"""
-
-    def area(self):
-        """calculated area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validated value"""
-        b = type(value)
-        if b is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class Rectangle inherits BaseGeometry"""
-
-    def __init__(self, width, height):
-        """initialization of privates"""
-        self.integer_validator('width', width)
-        self.__width = width
-        self.integer_validator('height', height)
-        self.__height = height
-
-    def area(self):
-        """implementing area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """return a str representation"""
-        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
+"""Write a class Square that inherits from Rectangle"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Class Square inherits Rectangle"""
+    """rectangle square class"""
     def __init__(self, size):
-        """initializer"""
-        self.integer_validator('size', size)
-        super().__init__(size, size)
+        """Method for initializing a square"""
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method returns area of a square"""
+        return self.__size ** 2
