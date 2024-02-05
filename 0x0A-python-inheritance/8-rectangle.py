8cd51c64e0af723907bdc36df1d84f163ff84339
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-02-05 17:52:03 +0000

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""class rectangle that inherits basegeometry"""
-
-
-class BaseGeometry:
-    """Instantiation"""
-
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validate"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""Rectangle class Module"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class Rectangle inherits BaseGeometry"""
-
+    """Rectangle class"""
     def __init__(self, width, height):
-        """initialization of width and height"""
-        self.integer_validator('width', width)
+        """Initilize rectangle method"""
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator('height', height)
+        self.integer_validator("height", height)
         self.__height = height
