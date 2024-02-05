207ac5e68e1acc3fd3a6143a1be3a0375a48290e
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-02-05 18:08:25 +0000

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Returns true if object if instance of a class that
-inherited specified class"""
+"""inherits_from method"""
 
 
 def inherits_from(obj, a_class):
-    """return True if object is an instance of a class that
-    inherited specified class or false otherwise"""
-    if issubclass(obj.__class__, a_class) is True:
-        if obj.__class__ is not a_class:
-            return True
-    else:
-        return False
+    """returns True if the object is an instance of a class that
+    inherited the specified class;
+    otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
