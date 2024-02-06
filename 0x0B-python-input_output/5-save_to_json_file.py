f859c8ccf027160038234b50b23903e0fec2760d
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-02-06 10:54:46 +0000

    Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,11 +12,6 @@ def save_to_json_file(my_obj, filename):
 
     Returns:
         type: JSON representation.
-    
-
-     print("type of json_object --> {}".format(type(json_object)))
-     print("type of my_obj --> {}".format(type(my_obj)))
-     print("type file name --> {}".format(type(filename)))
      writing to file"""
     with open(filename, 'w', encoding="utf-8") as f:
         # serializing json
