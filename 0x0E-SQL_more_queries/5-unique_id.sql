cf3c546fea9f1c0466ca4a46377edc4b4f06a8cf
Sophie Ododa <sophieododa@gmail.com>
2024-03-13 18:21:01 +0000

    sql more

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- script that creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);-- script that creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
