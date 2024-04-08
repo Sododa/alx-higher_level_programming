7815f898f98916279122bb33ce5e24237c383b16
Sophie Ododa <sophieododa@gmail.com>
2024-04-08 17:49:41 +0000

    object factor

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-const arrayLength = myLines.length;
-let j = 0;
-while (j < arrayLength) {
-  console.log(myLines[j]);
-  j++;
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
