44cc9141287c97aaba5c76f4aa0eaba0a7ef1c5d
Sophie Ododa <sophieododa@gmail.com>
2024-04-08 17:22:26 +0000

    Increment object

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = myLines.length;
+let j = 0;
+while (j < arrayLength) {
+  console.log(myLines[j]);
+  j++;
+}
+
