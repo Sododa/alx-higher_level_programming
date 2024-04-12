2f9773c2c26c8f0d928f235d60ab3a26498eede7
Sophie Ododa <sophieododa@gmail.com>
2024-04-12 07:58:17 +0000

    city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" prints all City objects from the database hbtn_0e_14_usa:
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Represents a city for a MySQL database.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
