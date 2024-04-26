7c8858e1891739f1888cd03854ca8e55b829554a
Sododa <sophieododa@gmail.com>
2024-04-26 20:03:26 +0000

    token removed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{Sododa}'.format(argv[1])
+    url = 'https://api.github.com/users/'.format(argv[1])
     r = requests.get(url,
                      auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
