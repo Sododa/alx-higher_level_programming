9e37aaacf693baf21b177e2c22a555a08de6a452
Sododa <sophieododa@gmail.com>
2024-05-30 10:20:22 +0000

    all

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
 // script that updates the text color of the <header> element to red (#FF0000)
 
-document.querySelector('head').style.color = '#FF0000';
+var header = document.querySelector('header');
+header.style.color = '#FF0000';
